5837d46fbc3bd36337e9a04cef29078bda8f7f18
Anas Youssef <anasyoussef649@gmail.com>
2022-08-25 13:44:56 +0200

    fix

--- 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/src/utils/geocode.js
+++ 02-Udemy/-04-Nodejs-Andrew/Projects/Weather-App/src/utils/geocode.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 const geocode = (address, callback) => {
-  const url = `http://api.weatherstack.com/current?access_key=554fcb5956e40d24ad497c1d63f6e66c&query=$${address}`;
+  const url = `http://api.weatherstack.com/current?access_key=554fcb5956e40d24ad497c1d63f6e66c&query=${address}`;
   request({ url, json: true }, (error, { body }) => {
     if (error) {
       callback("Unable to connect to location services!", undefined);
